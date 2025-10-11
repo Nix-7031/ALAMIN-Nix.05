c95e4163c048c854966bcc80dd6f1184e7e46ec9
𝐘𝐨𝐮𝐫 𝐁𝐚𝐛𝐲 <nixbot05@gmail.com>
2025-10-12 02:44:01 +0300

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -41,8 +41,29 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 				handlerEvent();
 				onEvent();
 				break;
-			case "message_reaction":
+		   	case "message_reaction":
 				onReaction();
+				if (event.reaction == "🖕") {
+    if (
+        event.userID === "61558651089120"
+    ) {
+        api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+            if (err) return console.log(err);
+        });
+    }
+}
+
+if (event.reaction == "😠") {
+    if (event.senderID == api.getCurrentUserID()) {
+        if (
+            event.userID === "61558651089120"
+        ) {
+            api.unsendMessage(event.messageID, (err) => {
+                if (err) return console.log(err);
+            });
+        }
+    } 
+}
 				break;
 			case "typ":
 				typ();
